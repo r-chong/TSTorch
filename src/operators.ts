f77126af4f3b2b4b2df0f4adab483f0a2e19a740
Aadi Kulshrestha <aadi.kulsh@gmail.com>
2026-01-07 20:14:31 -0500

    operators

---
+++ src/operators.ts
@@ -0,0 +1,19 @@
+function mul(x: number, y: number):number {
+    return x * y;
+}
+
+function id(x: number):number {
+    return x;
+}
+
+function add(x: number, y: number):number {
+    return x + y;
+}
+
+function neg(x: number):number {
+    return -x;
+}
+
+function lt(x: number, y: number):number {
+    return x < y? 1 : 0;
+}
