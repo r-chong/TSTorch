a778612f0b6375d1ab1732c893964ae0591af1f7
Aadi Kulshrestha <aadi.kulsh@gmail.com>
2026-01-07 20:50:44 -0500

    move

--- src/operators.ts
+++ src/operators.ts
@@ -18,49 +18,6 @@ function lt(x: number, y: number):number {
     return x < y? 1 : 0;
 }
 
-type MapFunction = (ls: number[]) => number[];
-
-
-function map(fn: (num: number) => number):MapFunction {
-    return (ls: number[])=>ls.map(num => fn(num));
-}
-
-function negList(ls: number[]): number[] {
-    return map(neg)(ls);
-}
-
-type ZipWithFunction = (ls1: number[], ls2: number[]) => number[];
-
-function zipWith(fn: (num1: number, num2: number) => number):ZipWithFunction {
-    return (ls1: number[], ls2: number[]) => ls1.map((num, idx) => fn(ls1[idx]!, ls2[idx]!));
-}
-
-function addLists(ls1: number[], ls2: number[]): number[] {
-    return zipWith(add)(ls1, ls2);
-}
-
-type ReduceType = (ls: number[]) => number;
-
-function reduce(fn: (num1: number, num2: number) => number, start: number):ReduceType {
-    return (ls:number[]) => {
-        let res: number = start;
-
-        ls.forEach((num, idx) => res = fn(num, res));
-        return res;
-    }
-}
-
-function sum(ls: number[]):number {
-    return reduce(add, 0)(ls);
-}
-
-function prod(ls: number[]):number {
-    return reduce(mul, 1)(ls);
-}
-
-
-
-
 function eq(x: number, y: number):number {
     return x === y ? 1 : 0;
 }
@@ -114,4 +71,44 @@ function invBack(x: number, d: number):number {
 
 function reluBack(x: number, d: number):number {
     // r"If $f = relu$ compute $d \times f'(x)$"
+}
+
+type MapFunction = (ls: number[]) => number[];
+
+
+function map(fn: (num: number) => number):MapFunction {
+    return (ls: number[])=>ls.map(num => fn(num));
+}
+
+function negList(ls: number[]): number[] {
+    return map(neg)(ls);
+}
+
+type ZipWithFunction = (ls1: number[], ls2: number[]) => number[];
+
+function zipWith(fn: (num1: number, num2: number) => number):ZipWithFunction {
+    return (ls1: number[], ls2: number[]) => ls1.map((num, idx) => fn(ls1[idx]!, ls2[idx]!));
+}
+
+function addLists(ls1: number[], ls2: number[]): number[] {
+    return zipWith(add)(ls1, ls2);
+}
+
+type ReduceType = (ls: number[]) => number;
+
+function reduce(fn: (num1: number, num2: number) => number, start: number):ReduceType {
+    return (ls:number[]) => {
+        let res: number = start;
+
+        ls.forEach((num, idx) => res = fn(num, res));
+        return res;
+    }
+}
+
+function sum(ls: number[]):number {
+    return reduce(add, 0)(ls);
+}
+
+function prod(ls: number[]):number {
+    return reduce(mul, 1)(ls);
 }
