245c6d1ef830ab0e318a12854df4d8a01e360418
r-chong <dev.reese.chong@gmail.com>
2026-01-21 12:33:10 -0500

    merge demo index.ts between dispatch parity test and 2 layer linear model

--- packages/demo/index.ts
+++ packages/demo/index.ts
@@ -1,4 +1,29 @@
 import { runDispatchParity } from "./dispatch_parity.js";
+import { Scalar, datasets } from "tstorch";
 
-const result = runDispatchParity();
-console.log(result);
+console.log(Scalar);
+
+class Network {
+    
+};
+
+class Linear {
+
+};
+
+class ScalarTrain {
+    constructor(hiddenLayers: number) {
+        this.hiddenLayers = hiddenLayers;
+        this.model = new Network(hiddenLayers)
+    }
+}
+
+function main() {
+    const PTS = 50;
+    const HIDDEN = 2;
+    const RATE = 0.5;
+    const data = datasets["Simple"](PTS);
+    const st = new ScalarTrain(HIDDEN).train(data, RATE);
+    const result = runDispatchParity();
+    console.log(result);
+}
