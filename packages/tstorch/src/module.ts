15e4064e5845ff4faab813f033dd8dfccfbeca92
r-chong <dev.reese.chong@gmail.com>
2026-02-16 11:44:52 -0500

    create model parameter class

---
+++ packages/tstorch/src/module.ts
@@ -0,0 +1,40 @@
+/* 
+Modules form a tree that store parameters and other
+submodules. They make up the basis of neural network stacks.
+
+Attributes:
+    modules : Storage of the child modules
+    parameters : Storage of the module's parameters
+    training : Whether the module is in training mode or evaluation mode
+*/
+
+export class Module {
+    private modules: Record<string, Module> = {};
+    private parameters: Record<string, BaseParameter> = {};
+    training: boolean = true;
+
+    constructor() {}
+}
+
+// Non-generic base class to type Parameter class yet not Module class
+export abstract class BaseParameter {
+    name?: string;
+}
+
+// TODO: default T=Tensor when merging into Tensor
+export class Parameter<T> extends BaseParameter {
+    value: T;
+
+    constructor(value: T, name?: string) {
+        super();
+        this.value = value;
+
+        if (name) {
+            this.name = name;
+        }
+    }
+
+    update(v: T) {
+        this.value = v;
+    }
+}
