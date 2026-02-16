8d168709f9c6e7f08e5f14a29f25160d8dbe1411
r-chong <dev.reese.chong@gmail.com>
2026-02-16 17:07:54 -0500

    create function to zero out gradients

--- packages/tstorch/src/optimizer.ts
+++ packages/tstorch/src/optimizer.ts
@@ -8,3 +8,30 @@ export class Optimizer<T> {
         this.parameters = parameters;
     }
 }
+
+export class SGD<T> extends Optimizer<T> {
+    lr: number;
+
+    constructor(parameters: Parameter<T>[], lr: number = 1.0) {
+        super(parameters);
+        this.lr = lr;
+    }
+
+    zeroGrad() {
+        for (let p of this.parameters) {
+            if (!p.value || typeof p.value !== 'object') {
+                continue;
+            }
+            if ("derivative" in p.value) { 
+                if (p.value.derivative !== null && p.value.derivative !== undefined) {
+                    p.value.derivative = null;
+                }
+            }
+            if ("grad" in p.value) { 
+                if (p.value.grad !== null && p.value.grad !== undefined) {
+                    p.value.grad = null;
+                }
+            }
+        }
+    }
+}
